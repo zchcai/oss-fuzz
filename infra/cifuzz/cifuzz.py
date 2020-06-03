6ca7bb018977e4675bdd2cc44edf8a5f7f762f93
Zhicheng Cai <16498538+zchcai@users.noreply.github.com>
2020-06-03 18:01:54 -0500

    Revert "sync (#5)"
    This reverts commit da66a25cc75eaa43ead69100392ef59e11d23812.

--- infra/cifuzz/cifuzz.py
+++ infra/cifuzz/cifuzz.py
@@ -136,16 +136,9 @@ def build_fuzzers(project_name,
 
   # Build Fuzzers using docker run.
   command = [
-      '--cap-add',
-      'SYS_PTRACE',
-      '-e',
-      'FUZZING_ENGINE=' + DEFAULT_ENGINE,
-      '-e',
-      'SANITIZER=' + DEFAULT_SANITIZER,
-      '-e',
-      'ARCHITECTURE=' + DEFAULT_ARCHITECTURE,
-      '-e',
-      'FUZZING_LANGUAGE=c++',  # FIXME: Add proper support.
+      '--cap-add', 'SYS_PTRACE', '-e', 'FUZZING_ENGINE=' + DEFAULT_ENGINE, '-e',
+      'SANITIZER=' + DEFAULT_SANITIZER, '-e',
+      'ARCHITECTURE=' + DEFAULT_ARCHITECTURE
   ]
   container = utils.get_container_name()
   if container:
@@ -251,14 +244,9 @@ def check_fuzzer_build(out_dir):
     return False
 
   command = [
-      '--cap-add',
-      'SYS_PTRACE',
-      '-e',
-      'FUZZING_ENGINE=' + DEFAULT_ENGINE,
-      '-e',
-      'SANITIZER=' + DEFAULT_SANITIZER,
-      '-e',
-      'ARCHITECTURE=' + DEFAULT_ARCHITECTURE,
+      '--cap-add', 'SYS_PTRACE', '-e', 'FUZZING_ENGINE=' + DEFAULT_ENGINE, '-e',
+      'SANITIZER=' + DEFAULT_SANITIZER, '-e',
+      'ARCHITECTURE=' + DEFAULT_ARCHITECTURE
   ]
 
   # Set ALLOWED_BROKEN_TARGETS_PERCENTAGE in docker if specified by user.
